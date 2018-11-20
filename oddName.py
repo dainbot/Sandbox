3508c9c083f5bf2da25363e0791740035027d633
Dain <dain.park@my.jcu.edu.au>
2018-11-20 10:03:50 +0800

    Initial commit

---
+++ oddName.py
@@ -0,0 +1,13 @@
+"""Input name and print odd characters"""
+
+name = input("Enter name:- ")
+
+#check that name is not blank
+while len(name)<=0:
+    print("Name is blank, enter again ")
+    name=input("Enter name:- ")
+#pritn odd character in the name
+print(name[::2])
+#print odd characters in the name
+for i in range(0,len(name),2):
+    print(name[i])
